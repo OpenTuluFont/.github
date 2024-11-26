c4183c5102d780831dd3c8eec560d56c731d4c3c
Deepak Padukone M <deepakpadukone20@live.com>
2024-11-26 21:04:30 +0000

    Create info.js

---
+++ info.js
@@ -0,0 +1,17 @@
+[{
+    path:'https://github.com/OpenTuluFont/baravu/raw/refs/heads/main/baravu.otf',
+    filename:'Baravu.otf',
+    name:'Baravu',
+    version:'2.0.0',
+    size:'112kb',
+    creator:'Deepak & Prahlad',
+    desc:`The second version of Baravu font that was created for Unicode submission`,
+},{
+    path:'https://github.com/OpenTuluFont/mandara/raw/refs/heads/main/XmMdar(mandara)v1(RL).ttf',
+    filename:'Mandara.ttf',
+    name:'Mandara',
+    version:'1.0.0',
+    size:'112kb',
+    creator:'Prahlad',
+    desc:``,
+}]
